01064d6f7b6ef3156c785991c63131cb35454e63
Ehsan Ahmad <58270856+EhsanWebDev@users.noreply.github.com>
2025-11-04 15:12:29 +0500

    Changed api

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,18 +1,22 @@
 import 'dart:convert';
 
-// A function to easily parse a list of users
 List<User> userListFromJson(String str) =>
     List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
 class User {
   final int id;
-  final String name;
+  final String name; // Our app's UI still just wants a single 'name' string
   final String email;
 
   User({required this.id, required this.name, required this.email});
 
-  // Factory constructor to parse JSON
-  // We are only taking the fields we need
-  factory User.fromJson(Map<String, dynamic> json) =>
-      User(id: json["id"], name: json["name"], email: json["email"]);
+  // --- THE MODEL'S PARSING LOGIC IS UPDATED ---
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["id"],
+    // We combine 'firstname' and 'lastname' from the nested
+    // 'name' object into our single 'name' variable.
+    name: "${json["name"]["firstname"]} ${json["name"]["lastname"]}",
+    email: json["email"],
+  );
+  // --- END OF CHANGE ---
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,28 +1,24 @@
 import 'package:http/http.dart' as http;
-import '../models/user_model.dart'; // <-- Import your model
+import '../models/user_model.dart';
 
 class ApiService {
-  static const String _usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  // --- THIS IS THE ONLY CHANGE IN THIS FILE ---
+  static const String _usersUrl = 'https://fakestoreapi.com/users';
+  // --- END OF CHANGE ---
 
-  // Static method so we don't have to instantiate the class
   static Future<List<User>> fetchUsers() async {
     try {
       final response = await http.get(Uri.parse(_usersUrl));
 
       if (response.statusCode == 200) {
-        // If the server returns a 200 OK response,
-        // then parse the JSON.
         final List<User> users = userListFromJson(response.body);
         return users;
       } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
         throw Exception(
           'Failed to load users (Status code: ${response.statusCode})',
         );
       }
     } catch (e) {
-      // Handle network errors or parsing errors
       throw Exception('Failed to load users: $e');
     }
   }
